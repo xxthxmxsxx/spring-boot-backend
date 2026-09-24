28b4d0abd4c921a53ad80737be7c58b1bdc89de9
xxthxmxsxx25 <thomasbryant1313@gmail.com>
2026-09-24 13:24:51 +0100

    committing base files

---
+++ src/main/java/com/notes/notesbackend/NotesbackendApplication.java
@@ -0,0 +1,13 @@
+package com.notes.notesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesbackendApplication.class, args);
+	}
+
+}
